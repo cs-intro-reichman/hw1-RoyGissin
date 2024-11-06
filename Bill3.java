5fdfaecc840ef5013b092e7b329abf7f368633d6
רועי גיסין <roygissin@h-MacBook-Pro-sl-rwy.local>
2024-11-06 17:10:06 +0200

    homework week 1

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name1 = (args[0]);
+		String name2= (args[1]);
+		String name3= (args [2]);
+		int value= Integer.parseInt(args[3]);
+       double a=(double) value/3.0;
+	    a= Math.ceil(a);
+		System.out.println(name1 + " " + name2 + " " + name3 + " please pay: " + a + "shekels" );
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		double c= Double.parseDouble(args[0]);
+		double r= Double.parseDouble(args[1]);
+		double n= Double.parseDouble(args[2]); 
+       double m=(r/100)+1;
+		double x= Math.pow(m,n);
+		double FV= c*x;
+		System.out.println("The future value is: " +(int) FV);
 	}
+
 }
